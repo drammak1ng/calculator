3770196af592ff2539b3e4afda4a9bf0301bf4fc
drammak1ng <bognaroda26@gmail.com>
2024-11-11 15:39:30 +0300

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -7,10 +7,10 @@ public class Calculator {
         Main result = new Main();
 
         System.out.println("Введите арифметическое выражение:");
-        String expression = input.nextLine(); // Считываем выражение с консоли
-        String answer = result.calc(expression); // Вычисляем результат выражения
+        String expression = input.nextLine(); 
+        String answer = result.calc(expression); 
 
-        System.out.println("Результат: " + answer); // Вывод результата на консоль
+        System.out.println("Результат: " + answer); 
     }
 }
 
@@ -18,16 +18,16 @@ public class Calculator {
 
 
 class Main {
-    // Метод для вычисления арифметического выражения
+   
     public static String calc(String input) {
-        // Разбиваем строку на числа и операторы
+      
         String[] parts = input.split(" ");
 
 
 
-        int num1 = Integer.parseInt(parts[0]); // Преобразуем первую часть в целое число
-        char op = parts[1].charAt(0); // Извлекаем оператор
-        int num2 = Integer.parseInt(parts[2]); // Преобразуем вторую часть в целое число
+        int num1 = Integer.parseInt(parts[0]); 
+        char op = parts[1].charAt(0); 
+        int num2 = Integer.parseInt(parts[2]); 
 
         int result;
         for (int i = 1; i < parts.length; i++) {
@@ -72,6 +72,6 @@ class Main {
                 return "Ошибка: некорректный оператор!";
         }
 
-        return String.valueOf(result); // Возвращаем результат в виде строки
+        return String.valueOf(result); 
     }
 }
